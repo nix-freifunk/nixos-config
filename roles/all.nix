a3ace4b8733f41d55e6f646dfce37a01076cf587
Tom Herbers <mail@tomherbers.de>
2024-08-31 14:22:16 +0200

    all: networkd and nftables for all machines

--- roles/all.nix
+++ roles/all.nix
@@ -3,6 +3,10 @@
   # Enable the OpenSSH daemon.
   services.openssh.enable = true;
 
+  networking.useNetworkd = true;
+  networking.nftables.enable = true;
+  networking.usePredictableInterfaceNames = true;
+
   users.users.root.openssh.authorizedKeys.keys = [
    "ssh-ed25519 AAAAC3NzaC1lZDI1NTE5AAAAILp4HgDDRQYOp1xXPTUkqv83dZw+DGIj5jZdBzR2u57Y tom v6"
    "sk-ssh-ed25519@openssh.com AAAAGnNrLXNzaC1lZDI1NTE5QG9wZW5zc2guY29tAAAAIMiPPoELDHdbSRFIDU55751WYNh97bEgBKVEgx3aEvUzAAAACnNzaDp0b20tdjg= Tom-YubiKey5NFC-2"
